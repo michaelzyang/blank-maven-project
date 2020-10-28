c245cf4f6c2655745f713298757fcac836dae849
Michael Yang <myang2@cs.cmu.edu>
2020-10-28 15:10:46 -0400

    blank project

---
+++ src/main/java/edu/cmu/cc/main/Main.java
@@ -0,0 +1,15 @@
+package edu.cmu.cc.main;
+
+
+public class Main {
+
+    /**
+     * Main method.
+     *
+     * @param args run args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
